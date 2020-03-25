0d1edfe24be5f46f057919f4f9882c30e81ddcfb
JUAN VICTOR SERRUDO CHAVEZ <juan.vsc@gmail.com>
2020-03-25 16:41:33 -0400

    20200325 Clase 125. Localstorage persistencia de la información. IMPORTANTE.

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,15 +9,30 @@ export class DeseosService {
   listas: Lista[] = [];
 
   constructor() {
-    const lista1 = new Lista('Recolectar piedras del infinito');
-    const lista2 = new Lista('Héroes a desaparecer');
+    this.cargarStorage();
 
-    this.listas.push(lista1, lista2);
+    // const lista1 = new Lista('Recolectar piedras del infinito');
+    // const lista2 = new Lista('Héroes a desaparecer');
+
+    // this.listas.push(lista1, lista2);
   }
 
   crearLista( titulo: string ) {
     const nuevaLista = new Lista(titulo);
     this.listas.push( nuevaLista );
+    this.guardarStorage();
+  }
+
+  guardarStorage() {
+    localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage() {
+    if ( localStorage.getItem('data') ) {
+      this.listas = JSON.parse( localStorage.getItem('data') );
+    } else {
+      this.listas = [];
+    }
   }
 
 }
